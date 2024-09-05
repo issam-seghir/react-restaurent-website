acc3d2145711785c019d1f94543135a95e906355
issam-seghir <issamusma@hotmail.com>
2024-09-05 15:49:27 +0100

    styling and improve header

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { images } from '../../constants';
 
 const SubHeading = ({ title }) => (
-  <div style={{ marginBottom: '1rem' }}>
-    <p className="p__cormorant">{title}</p>
-    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
-  </div>
+	<div style={{ marginBottom: "1rem" }}>
+		<p className="p__cormorant" dangerouslySetInnerHTML={{ __html: title }}>
+		</p>
+		<img src={images.spoon} alt="spoon_image" className="spoon__img" />
+	</div>
 );
 
 export default SubHeading;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,18 +5,26 @@ import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div className="app__header app__wrapper section__padding" id="home">
-    <div className="app__wrapper_info">
-      <SubHeading title="Chase the new flavour" />
-      <h1 className="app__header-h1">The Key To Fine Dining</h1>
-      <p className="p__opensans" style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-      <button type="button" className="custom__button">Explore Menu</button>
-    </div>
+	<div className="app__header app__wrapper section__padding" id="home">
+		<div className="app__wrapper_info">
+			<SubHeading title="🐔 Bienvenue chez <span class='golden'>O'Poulet </span>🐔" />
+			<h1 className="app__header-h1">Crousti 100% </h1>
+			<h1 className="app__header-h1">Fait Maison !</h1>
+			<p className="p__opensans" style={{ margin: "2rem 0" }}>
+				Chez <span className="golden">O'Poulet</span>, nous avons fait le choix du{" "}
+				<span className="golden">fait maison</span> pour vous offrir le meilleur du poulet rôti.<br/> Que ce soit les
+				épices savamment dosées, les marinades riches en saveurs, ou nos accompagnements uniques — tout est
+				préparé sur place avec passion et savoir-faire.{" "}
+			</p>
+			<button type="button" className="custom__button">
+				Explore Menu
+			</button>
+		</div>
 
-    <div className="app__wrapper_img">
-      <img src={images.welcome} alt="header_img" />
-    </div>
-  </div>
+		<div className="app__wrapper_img">
+			<img src={"https://assets.lummi.ai/assets/QmVow9RLvfYE5Fky5HZV2Axo1GKU7919Jh5eEsUqYAjrNT?auto=format&w=1500"} alt="header_img" />
+		</div>
+	</div>
 );
 
 export default Header;
